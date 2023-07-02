666886058d43383e6a718c3b364146d3dc6820b7
JeanetteMarieEnsley <jeanette.m.ensley@gmail.com>
2023-07-02 12:54:41 -0500

    adding content to the home page

--- __tests__/home.test.jsx
+++ __tests__/home.test.jsx
@@ -4,7 +4,28 @@ import React from "react";
 import Home from "../src/app/page";
 
 describe("Home page", () => {
-  it("Should render", () => {
+  it("Should display home page image", () => {
     render(<Home />);
+
+    const image = screen.queryByRole('img', {name: 'enchanted book'})
+    expect(image).toBeInTheDocument()
   });
+
+  it("Should contain heading for the home page", () => {
+    render(<Home />)
+
+    const heading = screen.getByRole('heading', {name: 'Hello Adventurer'})
+    expect(heading).toBeInTheDocument()
+  })
+  it("Should contain paragraph description for the home page", () => {
+    render(<Home />)
+    const paragraphMessage = `Are you ready to embark on your quest for Knowledge? Pick a path to follow and make sure to add your findings. Together we can level up our programming skills and rise up the ranks.`
+    const paragraphText = screen.getByText((text) => text.startsWith('Are you ready'))
+    expect(paragraphText.textContent).toBe(paragraphMessage)
+  })
+  it("Should have a button on page", () => {
+    render(<Home />)
+    const button = screen.getByRole('button', {name: 'Pick your Quest'})
+    expect(button).toBeInTheDocument()
+  })
 });
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,24 @@
 import React from "react";
+import enchanted_book from '../../public/enchanted_book.png'
+import style from "./page.module.css"
+import Image from 'next/image'
 
 export default function Home() {
-  return <div>Home Page</div>;
+
+ 
+  return (
+    <div className={style.container}>
+      <div className={style.item}>
+        <h1 className={style.title}>Hello Adventurer</h1>
+        <p className={style.desc}>Are you ready to embark on your quest for Knowledge? 
+          Pick a path to follow and make sure to add your findings. 
+          Together we can level up our programming skills and rise up the ranks.
+          </p>
+          <button className={style.button}>Pick your Quest</button>
+      </div>
+      <div className={style.item}>
+      <Image width={575} height={500} src={enchanted_book} alt="enchanted book" className={style.img} />
+      </div>
+    </div>
+  )
 }
